cea33d303b0aab1c7a479960191fe8e3f231782d
S I M O N. P <spau@hotmail.de>
2021-06-26 17:09:32 +0200

    Add main body

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,23 @@
+#include "user.h"
 #include <iostream>
-
 using namespace std;
+
 int main() {
-	cout << "hello world" << endl;
+	string username_input;
+	string password_input;
+
+	cout <<"**************************       Login/Registration      **************************" << endl;
+	cout <<"                                     Username: ";
+	cin>>username_input;
+	//validate input -> call user class and check for user
+	cout<<"                                      Password: ";
+	cin>>password_input;
+
+	cout<<endl;
+	username_input.clear();
+	password_input.clear();
+	//check password -> match regex or right password?
+
 	return 0;
 }
 
