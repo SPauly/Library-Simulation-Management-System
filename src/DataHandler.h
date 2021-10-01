9b28d311e9d3154a12bcae6374d4e2c064a27e8d
S I M O N. P <spau@hotmail.de>
2021-10-01 16:37:34 +0200

    Added CSV File class

--- src/DataHandler.h
+++ src/DataHandler.h
@@ -3,13 +3,9 @@
 #include <vector>
 #include <iostream>
 #include <string>
+#include <sstream>
 
-struct user_id{
-    std::string username;
-    std::string password;
-};
-
-class DataHandler{
+class DataHandler {
     std::string data_directory = "E:/Simon/Documents/Visual Studio 2017/Projects/GetIntoCPPagain/Data/Data.csv";
     std::ifstream read_data_file;
     std::ofstream write_data_file;
@@ -21,4 +17,24 @@ public:
     DataHandler();
     ~DataHandler();
     user_id find_user(const std::string&); 
+};
+
+class CSVfile {
+    std::string m_line;
+    std::vector<int> m_data;
+
+public:
+    std::string_view operator[](std::size_t index) const {
+        return std::string_view(&m_line[m_line[index]+1], m_data[index+1] - (m_data[index] + 1));
+    }
+    std::size_t size() const{
+        return m_data.size() -1;
+    }
+    void readNextRow(std::istream&);
+
+};
+
+struct user_id{
+    std::string username;
+    std::string password;
 };
