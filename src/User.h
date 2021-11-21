d3579d973ee63894d1a27b87b25cc3b2430b31b2
S I M O N. P <spau@hotmail.de>
2021-11-21 01:32:35 +0100

    New User Layout

--- src/User.h
+++ src/User.h
@@ -5,52 +5,94 @@
 #include <regex>
 #include <random>
 
-#define log(x) std::cout << x
+namespace user
+{
+    #define log(x) std::cout << x
 
-class Userinfo{
-private:
-    unsigned int m_next_position = 0;
+    class Error : public std::runtime_error
+    {
+    public:
+        Error(const std::string &msg) : std::runtime_error(std::string("User: ").append(msg)) {}
+    };
 
-    std::string_view m_ID;
-    std::string m_user_name = "";
+    enum _Openmode {
+        _Openmask = 0x4C
+    };
 
-    std::string m_path_userinfo {fm::init_workingdir() + "Data\\Userinfo.txt"};
-    csv::Header m_bookheader{"BID,DATE,POSITION"};
-    std::vector<csv::Row> mvec_books;
-    std::vector<csv::Row> mvec_owned;
-    std::vector<csv::Row> mvec_published;
+    static constexpr _Openmode user = (_Openmode)0x55; //U
+    static constexpr _Openmode publisher = (_Openmode)0x50; //P
+    static constexpr _Openmode admin = (_Openmode)0x41; //A
+    static constexpr _Openmode logout = (_Openmode)0x4C; //L
+    static constexpr _Openmode failure = (_Openmode)0x0; //NULL
 
-    std::fstream m_userinfo_txt;
-public:
-    Userinfo();
-    ~Userinfo();
+    enum _userstate
+    { 
+        _Statmask = 0x17
+    };
+
+    static constexpr _userstate goodbit = (_userstate)0x0; //everything's fine
+	static constexpr _userstate incobit = (_userstate)0x1; //incosistency bit
+	static constexpr _userstate failbit = (_userstate)0x2; //failure
+	static constexpr _userstate badbit = (_userstate)0x4;  //Input error
+
+    struct _ID {
+        unsigned int _ui_id = 0;
+        _Openmode _token = user;
+        std::string _string_id = "" + _token;
+    };
+    _ID& _create_ID(_ID&, int, int);
+
+    struct _dimensions {
+        unsigned int beg = 0;
+        unsigned int end = 0;
+        unsigned int space = end - beg;
+        unsigned int freespace = 0;
+    };
+
+    struct name {
+        std::string firstname = "";
+        std::string lastname = "";
+        std::string_view fullname = firstname + " " + lastname;
+    };
+
+    _Openmode& _login(const User&);
+    _Openmode& _logout(const User&);
+    //bool m_user_request(); //deals with authentification of the user
+    //bool m_create_user();  //deals with creating a new user
     
-    bool create_user_info(std::string_view);
-    bool load_userinfo(std::string_view);
+    class User
+    { //holds the user and is responsible for login, logout and activity log -> gets automatically deleted with logout
+    private:
+        _userstate m_state = goodbit;
+        _Openmode m_mode = failure; //flag to indicate wheather user is logged in or not
+        
+        _dimensions m_dimensions;
 
-    std::string_view get_name();
-};
+        _ID m_ID; //User ID UXXXXXX
+        name m_name_user;
 
-class User { //holds the user and is responsible for login, logout and activity log -> gets automatically deleted with logout
-private: 
-    bool m_login_flag;          //flag to indicate wheather user is logged in or not
-    std::string *mptr_username = nullptr; //username of the current user gets deleted after loggin
-    std::string *mptr_password = nullptr; //password entered by user also gets deleted after authenticating login
-    std::string m_ID; //User ID UXXXXXX
+        std::string m_path_userinfo{fm::init_workingdir() + "Data\\Userinfo.txt"};
+        csv::Header m_bookheader{"BID,DATE,POSITION"};
 
-    csv::Header m_userfile_header{"USERNAME,PASSWORD,UID"};
-    std::string m_path_userfile {fm::init_workingdir() + "Data\\Userfile.csv"};
-    csv::CSVParser *mptr_csv_parser = nullptr; 
-    Userinfo* mptr_userinfo = nullptr;            
+        std::vector<csv::Row> mvec_books;
+        std::vector<csv::Row> mvec_owned;
+        std::vector<csv::Row> mvec_published; 
 
-protected:
-    bool m_user_request(); //deals with authentification of the user
-    bool m_create_user(); //deals with creating a new user 
-    std::string_view m_create_ID(int,int);
-public:
-    User();
-    ~User();
-    Userinfo& login();
-    bool is_logged();
-    void logout();               
-};
+        std::fstream m_userinfo_txt;
+
+    public:
+        User();
+        ~User();
+
+        _Openmode& get_mode();
+        _userstate& get_state();
+        std::string_view get_name();
+        _ID& get_id();
+
+        bool create_user_info(std::string_view);
+        bool load_userinfo(std::string_view);
+    };
+
+
+
+}
