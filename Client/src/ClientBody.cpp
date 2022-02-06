af40e64c0219c88b6b164c61fe37dcbae7ca7791
SPauly <spau@hotmail.de>
2022-02-06 16:54:14 +0100

    Current State

---
+++ Client/src/ClientBody.cpp
@@ -0,0 +1,1 @@
+#include "ClientBody.h"
--- src/LibMsg.h
+++ src/LibMsg.h
@@ -3,35 +3,20 @@
 
 namespace LIBMSG {
 
-    enum class _UserType : uint8_t {
-        _admin,
-        _publisher,
-        _user
-    };
-
-    enum class _Id : uint8_t {
-        _id
-    };
-
     enum class _MessageType : uint8_t {
         _request,
         _answer
         //add some more
     };
 
-    struct _MessageBody {
-        char* msg;
-
-        _MessageBody(const char*);
-        ~_MessageBody();
+    struct _Header {
+        _MessageType mt{};
+        uint32_t size = 0;
     };
 
     class LibMsg {
-        const char* msg;
-        _UserType m_ut;
-        _Id m_id;
-        _MessageType m_mt;
-        _MessageBody m_msgb;
+        _Header m_header{};
+        
     public:
         
     };
